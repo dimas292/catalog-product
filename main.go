c49a07e09ca977c62ab3d35ef0ab4828717b3b1c
dimas <dimasixc22@gmail.com>
2024-08-29 09:16:29 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main(){
+
+}
+
+// base.go
+// │   │   ├── controller-web.go
+// │   │   ├── model.go
+// │   │   ├── params.go
+// │   │   ├── repository.go
+// │   │   └── service.go
