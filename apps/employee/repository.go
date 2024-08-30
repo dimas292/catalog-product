ece6fb338e323a1cc714e1a4784ef3c316701c0f
dimas <dimasixc22@gmail.com>
2024-08-30 08:02:15 +0700

    feat : repository layer

--- apps/employee/repository.go
+++ apps/employee/repository.go
@@ -1,1 +1,42 @@
 package employee
+
+import (
+	"context"
+	"database/sql"
+)
+
+type repository struct {
+	db *sql.DB
+}
+
+func newRepository(db *sql.DB) repository {
+	return repository{db: db}
+}
+
+func (r repository) findALlEmployees(ctx context.Context) (res []Employee, err error) {
+
+	query := `
+	SELECT id, name, address, nip, cretaed_at
+	FROM employees
+	`
+
+	row, err := r.db.QueryContext(ctx, query)
+	if err != nil {
+		return
+	}
+
+	defer row.Close()
+
+	for row.Next() {
+
+		var emp = Employee{}
+
+		if err := row.Scan(&emp.Id, &emp.Name, &emp.Address, &emp.NIP, &emp.CreatedAt); err != nil {
+			return []Employee{}, nil
+		}
+
+		res = append(res, emp)
+	}
+
+	return res, nil
+}
