5e704aed215cc7254496388b63cd5fe2d7990a6e
dimas <dimasixc22@gmail.com>
2024-08-30 07:46:06 +0700

    feat : service and params

--- apps/employee/params.go
+++ apps/employee/params.go
@@ -1,2 +1,15 @@
 package employee
 
+type renderWeb struct{
+	Title string
+	Data interface{}
+}
+
+type listEmployeeResponse struct {
+	Id        int
+	Name      string
+	Address   string
+	NIP       string
+	CreatedAt string
+
+}
--- apps/employee/service.go
+++ apps/employee/service.go
@@ -1,3 +1,44 @@
 package employee
 
+import (
+	"context"
+	"log"
+	"time"
+)
+
+type repositoryContract interface{
+	findAllEmployees(ctx context.Context)(res []Employee, err error)
+}
+
+type service struct{
+	repo repositoryContract
+}
+
+func newService(repo repositoryContract) service{
+	return service{repo: repo}
+}
+
+func(s service) listEmployee(ctx context.Context) (employees []listEmployeeResponse, err error){
+
+	resp, err := s.repo.findAllEmployees(ctx)
+	if err != nil {
+		log.Println("[listEmployees, findALlEmployee] Error", err)
+		return []listEmployeeResponse{}, nil
+	}
+
+	for _, v := range resp {
+		var createdAt = v.CreatedAt.Format(time.DateOnly)
+		emp := listEmployeeResponse{
+			Id: v.Id,
+			Name: v.Name,
+			NIP: v.NIP,
+			Address: v.Address,
+			CreatedAt: createdAt,
+		}
+
+		employees = append(employees, emp)
+	}
+
+	return employees, nil
+}
 
